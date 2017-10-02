344440d118b86c9a29efcb6379322f924562a0d1
Behzad Samadi <behzad.samadi@gmail.com>
2017-10-02 16:42:34 -0400

    Update the Maple Cloud information

--- src/build.bat
+++ src/build.bat
@@ -1,5 +1,5 @@
 @echo off
 REM Add the "bin.X86_64_WINDOWS" folder of Maple to the PATH
 cmaple build.mm
-mv NMPCToolkit.maple ..\install
-mv NMPCToolkit.help ..\install
+mv NMPCToolkit.maple ..\build
+mv NMPCToolkit.help ..\build
