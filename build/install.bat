0a7ae4b5bb44063f0f92eeb514d619b9cbcb9e5c
Behzad Samadi <bsamadi@maplesoft.com>
2018-03-07 10:28:08 -0500

    Adding mkdir

--- build/install.bat
+++ build/install.bat
@@ -1,2 +1,3 @@
+mkdir %USERPROFILE%\maple\toolbox\NMPCToolkit\lib
 copy NMPCToolkit.maple %USERPROFILE%\maple\toolbox\NMPCToolkit\lib\
 copy NMPCToolkit.help %USERPROFILE%\maple\toolbox\NMPCToolkit\lib\
